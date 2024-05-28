a508471d03f401dd6bcecafb8ed89399f81d9fdf
Vaishali Srivastava <vaishali.srivastava1204@gmail.com>
2024-05-28 16:50:45 +0530

    edited

--- script.js
+++ script.js
@@ -1,97 +1,46 @@
-// for button 
-document.querySelector('.button').onmousemove = function (e) {
-
-	var x = e.pageX - e.target.offsetLeft;
-	var y = e.pageY - e.target.offsetTop;
-
-	e.target.style.setProperty('--x', x + 'px');
-	e.target.style.setProperty('--y', y + 'px');
-};
-
-
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
+$(document).ready(function(){
+  $(window).scroll(function(){
+      // sticky navbar on scroll script
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }else{
+          $('.navbar').removeClass("sticky");
       }
-    }
-  }
-  
-  window.addEventListener("scroll", reveal);
 
-  // js for skill section 
-  const scrollers = document.querySelectorAll(".scroller");
-
-// If a user hasn't opted in for recuded motion, then we add the animation
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-addAnimation();
-}
-
-function addAnimation() {
-scrollers.forEach((scroller) => {
-// add data-animated="true" to every `.scroller` on the page
-scroller.setAttribute("data-animated", true);
-
-// Make an array from the elements within `.scroller-inner`
-const scrollerInner = scroller.querySelector(".scroller__inner");
-const scrollerContent = Array.from(scrollerInner.children);
-
-scrollerContent.forEach((item) => {
-  const duplicatedItem = item.cloneNode(true);
-  duplicatedItem.setAttribute("aria-hidden", true);
-  scrollerInner.appendChild(duplicatedItem);
-});
-});
-}
-
-
-// for project section 
-let section = document.querySelectorAll("section");
-let menu = document.querySelectorAll("header nav a");
-
-window.onscroll = () => {
-  section.forEach((i) => {
-    let top = window.scrollY;
-    let offset = i.offsetTop - 150;
-    let height = i.offsetHeight;
-    let id = i.getAttribute("id");
-
-    if (top >= offset && top < offset + height) {
-      menu.forEach((link) => {
-        link.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
-      });
-    }
+      // scroll-up button show/hide script
+      if(this.scrollY > 500){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+          $('.scroll-up-btn').removeClass("show");
+      }
   });
-};
 
-// function reveal() {
-//   var reveals = document.querySelectorAll(".reveal");
+  // slide-up script
+  $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop: 0});
+      // removing smooth scroll on slide-up button click
+      $('html').css("scrollBehavior", "auto");
+  });
 
-//   for (var i = 0; i < reveals.length; i++) {
-//     var windowHeight = window.innerHeight;
-//     var elementTop = reveals[i].getBoundingClientRect().top;
-//     var elementVisible = 150;
+  $('.navbar .menu li a').click(function(){
+      // applying again smooth scroll on menu items click
+      $('html').css("scrollBehavior", "smooth");
+  });
 
-//     if (elementTop < windowHeight - elementVisible) {
-//       reveals[i].classList.add("active");
-//     } else {
-//       reveals[i].classList.remove("active");
-//     }
-//   }
-// }
+  // toggle menu/navbar script
+  $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  });
 
-// window.addEventListener("scroll", reveal);
+  // typing text animation script
+  var typed = new Typed(".typing", {
+      strings: ["Web Developer", "Programmer", "Designer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
 
-// // To check the scroll position on page load
-// reveal();
+
+  
+  });
